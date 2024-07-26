881756d90d5fb654082c3b86a2b58325cb6fd9b3
mad0021 <123090580+mad0021@users.noreply.github.com>
2024-07-26 13:00:07 +0200

    Add PHP svg

--- assets/pages/index.php
+++ assets/pages/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'>
     <link rel="stylesheet" href="./style.css">
 </head>
+
     <header class="header show-on-scroll">
         <div class="main-photo"></div>
         <div class="heading">
@@ -15,7 +17,9 @@
             <p><a id="header-btn" class="header-btn">Contact Me</a></p>
         </div>
     </header>
+
     <section id="about" class="dark-bg">
+
         <div class="about-summary">
             <h2 class="white-text">About me</h2>
             <p>
@@ -44,6 +48,7 @@
         </div>
 
         <div class="about-skills">
+
             <h3 class="white-text">Skills</h3>
                 <ul class="grid-list slide-left show-on-scroll">
                     <li>
@@ -83,9 +88,35 @@
                         <div class="svg-title-container">CSS</div>
                     </li>
 
+                    <li>
+                        <div class="about-svg-conatainer">
+                            <svg width="800px" height="80px" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
+                                <g>
+                                    <path d="M0,0 L256,0 L256,256 L0,256 L0,0 Z" fill="#F7DF1E"></path>
+                                    <path d="M67.311746,213.932292 L86.902654,202.076241 C90.6821079,208.777346 94.1202286,214.447137 102.367086,214.447137 C110.272203,214.447137 115.256076,211.354819 115.256076,199.326883 L115.256076,117.528787 L139.313575,117.528787 L139.313575,199.666997 C139.313575,224.58433 124.707759,235.925943 103.3984,235.925943 C84.1532952,235.925943 72.9819429,225.958603 67.3113397,213.93026" fill="#000000"></path>
+                                    <path d="M152.380952,211.354413 L171.969422,200.0128 C177.125994,208.433981 183.827911,214.619835 195.684368,214.619835 C205.652521,214.619835 212.009041,209.635962 212.009041,202.762159 C212.009041,194.513676 205.479416,191.592025 194.481168,186.78207 L188.468419,184.202565 C171.111213,176.81473 159.597308,167.53534 159.597308,147.944838 C159.597308,129.901308 173.344508,116.153295 194.825752,116.153295 C210.119924,116.153295 221.117765,121.48094 229.021663,135.400432 L210.29059,147.428775 C206.166146,140.040127 201.699556,137.119289 194.826159,137.119289 C187.78047,137.119289 183.312254,141.587098 183.312254,147.428775 C183.312254,154.646349 187.78047,157.568406 198.089956,162.036622 L204.103924,164.614095 C224.553448,173.378641 236.067352,182.313448 236.067352,202.418387 C236.067352,224.071924 219.055137,235.927975 196.200432,235.927975 C173.860978,235.927975 159.425829,225.274311 152.381359,211.354413" fill="#000000"></path>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="svg-title-container">JavaScript</div>
+                    </li>
+
+                    <li>
+                        <div class="about-svg-conatainer">
+                            <svg width="800px" height="80px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-label="PHP" role="img">
+                                <rect width="512" height="512" rx="15%" fill="#777bb3"></rect>
+                                <g stroke="#ffffff" stroke-width="6">
+                                    <path id="a" d="M155 202H97a3 3 0 0 0-3 2L67 345c0 3 3 3 3 3h30c3 0 3-2 3-2l7-35h27c42 0 63-28 68-53s1-56-50-56zm13 54c-4 19-17 27-35 27h-18l11-53h18c27 0 26 10 24 26z"></path>
+                                    <path d="M275 311c-4 0-3-4-3-4l12-61c0-8 6-16-18-15h-18l-15 78s0 3-4 3h-29c-3 0-3-3-3-3l28-141s0-2 4-2h30c3 0 2 3 2 3l-7 34h26c36 0 45 17 42 37l-13 69s0 3-5 3z"></path>
+                                    <use xlink:href="#a" x="239"></use>
+                                </g>                        </div>
+                        <div class="svg-title-container">PHP</div>
+                    </li>
 
                 </ul>
+
         </div>
+
     </section>
 </body>
 </html>
