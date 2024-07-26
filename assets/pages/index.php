1c20377fec68ca2724cecf568ddb5e042b98f986
mad0021 <123090580+mad0021@users.noreply.github.com>
2024-07-26 12:48:49 +0200

    Add CSS svg

--- assets/pages/index.php
+++ assets/pages/index.php
@@ -7,7 +7,6 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'>
     <link rel="stylesheet" href="./style.css">
 </head>
-
     <header class="header show-on-scroll">
         <div class="main-photo"></div>
         <div class="heading">
@@ -16,7 +15,6 @@
             <p><a id="header-btn" class="header-btn">Contact Me</a></p>
         </div>
     </header>
-
     <section id="about" class="dark-bg">
         <div class="about-summary">
             <h2 class="white-text">About me</h2>
@@ -73,6 +71,19 @@
                         </div>
                         <div class="svg-title-container">HTML</div>
                     </li>
+
+                    <li>
+                        <div class="about-svg-conatainer">
+                            <svg width="800px" height="80px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6 28L4 3H28L26 28L16 31L6 28Z" fill="#1172B8"/>
+                                <path d="M26 5H16V29.5L24 27L26 5Z" fill="#33AADD"/>
+                                <path d="M19.5 17.5H9.5L9 14L17 11.5H9L8.5 8.5H24L23.5 12L17 14.5H23L22 24L16 26L10 24L9.5 19H12.5L13 21.5L16 22.5L19 21.5L19.5 17.5Z" fill="white"/>
+                            </svg>
+                        </div>
+                        <div class="svg-title-container">CSS</div>
+                    </li>
+
+
                 </ul>
         </div>
     </section>
